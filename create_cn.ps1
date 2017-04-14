f82850aa43cef1476b8cc2b2f09c02f94a3d7bd8
ZIT WANG <mymisting@gmail.com>
2017-04-14 20:05:33 +0800

    完成脚本

--- create_cn.ps1
+++ create_cn.ps1
@@ -1,6 +1,8 @@
 $current_dir = $ExecutionContext.SessionState.Path.CurrentLocation
 
-git clone -b master --depth=1 https://github.com/mst-mrh/Miyuu.TmlModTextDumper.git
+Write-Host "Cloning patcher..."
+
+git clone -q --branch=master --depth=1 https://github.com/mst-mrh/Miyuu.TmlModTextDumper.git
 Set-Location Miyuu.TmlModTextDumper
 git submodule update --init --recursive
 appveyor-retry nuget restore Miyuu.TmlModTextDumper.sln
@@ -15,10 +17,11 @@ $params = @{modloaderversion="tModLoader v0.9.2.3";platform="w"}
 $mods = ((Invoke-WebRequest -Uri http://javid.ddns.net/tModLoader/listmods.php -Method POST -Body $params).Content | ConvertFrom-Json).modlist
 
 $m = $mods | Where-Object name -eq $modname
-
+$ver = $m.version
 $link = $m.download
-
 $name = $modname + ".tmod"
+
+Write-Host "Start downloading $modname $ver from $link"
 
 Try
 {
